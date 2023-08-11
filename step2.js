f788b480d9aa70f0afdce506d0bd4082334617e4
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2023-08-11 16:28:24 -0400

    first

---
+++ step2.js
@@ -0,0 +1,34 @@
+let fs = require('fs');
+let process = require('process');
+
+
+let axios = require('axios');
+
+function cat(path) {
+  fs.readFile(path, 'utf8', function(err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+
+async function webCat(url) {
+  try {
+    let resp = await axios.get(url);
+    console.log(resp.data);
+  } catch (err) {
+    console.error(`Error fetching ${url}: ${err}`);
+    process.exit(1);
+  }
+}
+
+let path = process.argv[2];
+if (path.slice(0, 4) === 'http') {
+  webCat(path);
+} else {
+  cat(path);
+}
